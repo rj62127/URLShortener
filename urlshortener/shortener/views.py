e6f3034a9753786ce61b35f175b0798c1f665bb8
rj62127 <rjha2.718@gmail.com>
2024-02-09 15:25:32 +0530

    Update views

--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -3,6 +3,9 @@ from .forms import ShortenURLForm
 from .models import ShortenedURL
 import shortuuid
 
+domain = "http://ec2-44-199-137-71.compute-1.amazonaws.com"
+#domain = "127.0.0.1"
+
 def shorten_url(request):
     
     if request.method == 'POST':
@@ -13,11 +16,11 @@ def shorten_url(request):
             try:
                 shorten_url = ShortenedURL.objects.get(original_url=original_url)
                 short_code = shorten_url.short_code
-                return render(request, 'shortener/success.html', {'short_url': f'http://127.0.0.1:8000/{short_code}'})
+                return render(request, 'shortener/success.html', {'short_url': domain+ ":8000/"+short_code})
             except ShortenedURL.DoesNotExist:
                 short_code = shortuuid.uuid()[:8]  # Use shortuuid library for short codes
                 ShortenedURL.objects.create(original_url=original_url, short_code=short_code)
-                return render(request, 'shortener/success.html', {'short_url': f'http://127.0.0.1:8000/{short_code}'})
+                return render(request, 'shortener/success.html', {'short_url': domain+ ":8000/"+short_code})
     else:
         form = ShortenURLForm()
 
